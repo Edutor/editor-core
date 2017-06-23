728d933427c740e747bcccb66a833b0d2275e6f3
Anders Kalhauge <anders@kalhauge.dk>
2017-06-23 12:12:01 +0200

    Examples of REST and Websocket implementations added

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # edutor-core
 Core module of the Edutor platform the new awesome autocorrection platform for Cphbusiness
+
+Examples of REST and Websocket implementations added
--- src/main/kotlin/dk/edutor/Greeting.kt
+++ src/main/kotlin/dk/edutor/Greeting.kt
@@ -1,10 +1,20 @@
 package dk.edutor
 
+import org.springframework.context.annotation.Configuration
+import org.springframework.messaging.handler.annotation.MessageMapping
+import org.springframework.messaging.handler.annotation.SendTo
+import org.springframework.messaging.simp.config.MessageBrokerRegistry
+import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.socket.config.annotation.*
 import java.util.concurrent.atomic.AtomicLong
 
 
 data class Greeting (val id: Long, val content: String)
+
+class Message() {
+  var name: String = "Killroy"
+  }
 
 @RestController
 class GreetingController {
@@ -15,3 +25,29 @@ class GreetingController {
     Greeting(counter.incrementAndGet(), "Hello $name you are #$counter")
 
   }
+
+@Controller
+class MessageController {
+  @MessageMapping("/hi")
+  @SendTo("/topic/greetings")
+  fun greeting(message: Message): Greeting {
+    Thread.sleep(1000L)
+    return Greeting(4711, "Hi ${message.name}!")
+    }
+  }
+
+@Configuration
+@EnableWebSocketMessageBroker
+open class WebSocketConfig : AbstractWebSocketMessageBrokerConfigurer() {
+
+  override fun configureMessageBroker(registry: MessageBrokerRegistry) {
+    registry.enableSimpleBroker("/topic")
+    registry.setApplicationDestinationPrefixes("/app")
+    }
+
+  override fun registerStompEndpoints(registry: StompEndpointRegistry) {
+    registry.addEndpoint("/gs-guide-websocket").withSockJS()
+    }
+
+  }
+
